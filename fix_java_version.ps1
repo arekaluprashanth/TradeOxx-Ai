3bc89b31c6cfcfe0bbea5ee283b304dfd206f10d
arekaluprashanth <prashu2242@gmail.com>
2026-07-13 23:38:21 +0530

    chore: clean up unwanted legacy documentation and test scripts

--- fix_java_version.ps1
+++
@@ -1,8 +0,0 @@
-$files = Get-ChildItem -Path 'android', 'node_modules\@capacitor\android' -Filter *.gradle -Recurse
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName
-    if ($content -match 'VERSION_21') {
-        $content = $content -replace 'VERSION_21', 'VERSION_17'
-        Set-Content $file.FullName $content
-    }
-}
